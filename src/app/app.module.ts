fb0d78d7223ae7124994dc3d64dd9648c2f882de
Valdemar26 <z_valdemar@ukr.net>
2017-06-01 11:09:18 +0300

    added three new components - albums, photos and photo-details

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { AlbumsComponent } from './albums/albums.component';
+import { PhotosComponent } from './photos/photos.component';
+import { PhotoDetailsComponent } from './photo-details/photo-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    AlbumsComponent,
+    PhotosComponent,
+    PhotoDetailsComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/photo-details/photo-details.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-photo-details',
+  templateUrl: './photo-details.component.html',
+  styleUrls: ['./photo-details.component.css']
+})
+export class PhotoDetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
